410e19240bf385712cb8a8da6b90238e5dc48b2e
jimmy-martin <jimmy.martin952@gmail.com>
2023-05-18 16:55:06 +0200

    Created all entities CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
+import { BeatsModule } from './beats/beats.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, UsersModule, BeatsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
